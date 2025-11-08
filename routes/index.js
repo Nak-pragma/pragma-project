1c4b9117f0955871fdcdac93d8368557c30ff57a
Nak-pragma <kintone@pragmaadvisory.net>
2025-11-08 20:44:11 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 // routes/index.js
 import express from "express";
 import openaiRouter from "./openai.js";
+import multiRoutes from "./multi.js";
 
 const router = express.Router();
 
 router.use("/openai", openaiRouter);
+router.use("/multi", multiRoutes);
 
 export default router;
